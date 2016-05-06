d6b232e0c4c4ffa5337bc8ae664b3d82f2e1fdfe
Andrew H <aho338@gmail.com>
2016-05-06 14:27:35 -0700

    Configure app for Heroku deployment

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,15 @@
 var Sequelize = require('sequelize');
-var password = require('../config/mysqlsetup.js');
+// var password = require('../config/mysqlsetup.js');
+var name = process.env.CLEARDB_DATABASE_NAME;
+var host = process.env.CLEARDB_DATABASE_HOST;
+var password = process.env.CLEARDB_DATABASE_PASSWORD;
+var username = process.env.CLEARDB_DATABASE_USERNAME;
 
 // Using ES2015:
 // import password from '../config/mysqlsetup.js';
 
-var db = new Sequelize('scenic', 'root', password, {
+var db = new Sequelize(name, username, password, {
+  host: host,
   define: {
     timestamps: false // true by default
   }
